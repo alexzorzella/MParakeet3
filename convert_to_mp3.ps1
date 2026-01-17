b9e9f4ce2f6ad9376f270ef37bcc603ae892c926
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-01-17 11:25:05 -0800

    Parameters are supplied by .ini files

--- convert_to_mp3.ps1
+++ convert_to_mp3.ps1
@@ -15,6 +15,27 @@ function Get-IniContent ($Path) {
     return $ini
 }
 
+# $FolderTrackCounts = @{}
+# $TotalTrackCount = 0
+# $TracksProcessed = 0
+
+# $FolderSizes = @{}
+
+# Get-ChildItem -Path $SourceFolder -Filter *.mp3 -Recurse | ForEach-Object {
+#     $escapedPath = [Regex]::Escape($SourceFolder)
+#     $RelativePath = $_.FullName -replace "^$escapedPath", ""
+#     $RelativePath = $RelativePath.TrimStart("\")
+	
+# 	$RelativeDirectory = Split-Path $RelativePath -Parent
+	
+# 	if(-not $FolderTrackCounts.ContainsKey($RelativeDirectory)) {
+# 		$FolderTrackCounts.Add($RelativeDirectory, 0)
+# 	}
+	
+# 	$FolderTrackCounts[$RelativeDirectory]++
+# 	$TotalTrackCount++
+# }
+
 $configFile = Join-Path $PSScriptRoot "config.ini"
 $configData = Get-IniContent -Path $configFile
 
@@ -23,25 +44,56 @@ $OutputFolder = $configData["music"]["output"].Trim('"')
 [int]$FolderTrackLimit = $configData["music"]["foldertracklimit"]
 [int]$MaxThreads = $configData["music"]["maxthreads"]
 
-Write-Host "Source: $SourceFolder, Output: $OutputFolder, FolderTrackLimit: $FolderTrackLimit, MaxThreads: $MaxThreads"
+$Continue = Read-Host -Prompt "Converting audio files in $SourceFolder to mp3. Outputting to $OutputFolder. Continue? (y/n)"
+
+if ($Continue.ToLower() -ne "y") {
+	return;
+}
 
 if (!(Test-Path $OutputFolder)) { New-Item -ItemType Directory -Path $OutputFolder }
 
-Get-ChildItem -Path $SourceFolder -Include *.m4a, *.opus -Recurse | ForEach-Object -Parallel {
+Get-ChildItem -Path $SourceFolder -Include *.m4a, *.opus, *.mp3, *.flac, *.wav -Recurse | ForEach-Object -Parallel {
+	$Shell = New-Object -ComObject Shell.Application
+    $originalName = $_.BaseName
+
     $RelativePath = $_.FullName.Replace($using:SourceFolder, "").TrimStart("\")
     $TargetFilePath = Join-Path $using:OutputFolder $RelativePath
-    $TargetFilePath = [System.IO.Path]::ChangeExtension($TargetFilePath, ".mp3")
+
+	if (-not $TargetFilePath -contains ".mp3") {
+		$TargetFilePath = [System.IO.Path]::ChangeExtension($TargetFilePath, ".mp3")
+	}
 
     $TargetDir = Split-Path $TargetFilePath
     if (!(Test-Path $TargetDir)) { New-Item -ItemType Directory -Path $TargetDir -Force }
 
-    Write-Host "Converting $($_.Name) to $TargetFilePath" -ForegroundColor Cyan
+	$FolderObj = $Shell.NameSpace($_.DirectoryName)
+    $FileObj = $FolderObj.ParseName($_.Name)
+    $currentTitle = $FolderObj.GetDetailsOf($FileObj, 21)
 
-    ffmpeg -i $_.FullName `
-        -codec:a libmp3lame -q:a 0 `
-        -map_metadata 0 `
-        -id3v2_version 3 `
-        -y $TargetFilePath
+    if ([string]::IsNullOrWhiteSpace($currentTitle)) {
+        $cleanTitle = $originalName -replace '\s\([a-z0-9]+_(?:Opus|AAC)\)$', ''
+        
+        Write-Host "Metadata missing. Copying $cleanTitle with metadata..." -ForegroundColor Yellow
+        
+        ffmpeg -i $_.FullName `
+            -c copy `
+            -map_metadata 0 `
+            -metadata title="$cleanTitle" `
+            -id3v2_version 3 `
+            -y $TargetFilePath 2>$null
+    }
+    else {
+        Write-Host "Metadata already exists for $currentTitle. Copying..." -ForegroundColor Gray
+        Copy-Item -Path $_.FullName -Destination $TargetFilePath -Force
+    }
+
+    # Write-Host "Converting $($_.Name) to $TargetFilePath" -ForegroundColor Cyan
+
+    # ffmpeg -i $_.FullName `
+    #     -codec:a libmp3lame -q:a 0 `
+    #     -map_metadata 0 `
+    #     -id3v2_version 3 `
+    #     -y $TargetFilePath
 } -ThrottleLimit $MaxThreads
 
 Write-Host "`nAll files converted!" -ForegroundColor Green
